c34b63106af25a236b1a923a8e9706cbef0fa284
demurre <y.drozdov130705@gmail.com>
2023-10-27 14:25:15 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# ToDo List
+
+![todolist](https://github.com/demurre/demurre/assets/117121382/f092e22c-c2fc-475e-a0a2-4aa180b97682)
+
+## Run locally
+
+```bash
+npm start
+```
+
+## Using:
+
+![js](https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black)
+![react](https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black)
---
+++ src/components/todoWrapper.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { TodoItem } from "./todo";
+import { TodoForm } from "./todoForm";
+import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./editForm";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <TodoForm addTodo={addTodo} />
+      {}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <TodoItem
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
